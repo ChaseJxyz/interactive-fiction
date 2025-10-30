35bfc439c4140299385f91372f4a3270f03dbae8
Chase J <contact@chasej.xyz>
2025-10-29 17:49:42 -0700

    i thought i was done for the day but i wanted to do more oop

--- IT 140/friend_crow.py
+++ IT 140/friend_crow.py
@@ -1,4 +1,44 @@
-# this is for all the helpy/physical manipulation things friend crow does (since you dont have hands)
+def help(): # Give player clues on what they can/should do
+	print("yes i am friend crow i am helping look i can even say stuff")
+	verbs()
 
-def help(word):
-	print("yes i am friend crow i am helping look i can even say", word,"/n")
+def verbs(): # Tell player what actions they can do
+	print("friend crow does something cute and says yes well you cant do much what with the hooves and all, but you have me! should i tell you what i can do?")
+	crow_answer = input("↦ How do you respond?\n").lower()
+	if crow_answer in ["yes","y","sure","please"]:
+		print("i can --insert list of verbs with newlines between--. but im not a mind reader so if you want me to take something specific, you have to tell me!\n\ncourse youre the one moving and looking at stuff, and you can read your own mind, but you still need to decide where to go or what to look at!")
+	else:
+		print("friend crow does something slightly less cute. well, okay! but im always here if you need help! just have to ask!")
+
+def eat(word): # Eat/drink, always fails
+	print("its not a good idea to eat",word,"cause plot reasons lol")
+
+def examine(word): # Look at room/item/scenary for flavor/plot/clues
+	print("youre gonna look up the descript for",word)
+
+def drop (word): # Inventory is one-way, so always fails
+	print("youre not allowed to do drop",word,"for plot reasons")
+
+def move(direction): # Update player's location to new room and calls initializations, if any
+	#TODO: make 1 char strings equal to whole words
+	print("this is the part where u go",direction)
+	print("this will be the init descript for the new room")
+
+def take(item): # Update item location from room to inventory.
+	if item in ["all","everything"]:
+		print("that's cheating lol do some reading")
+	else: #TODO: needs a way to fail if its a thing thats not in the lookup
+		print("you directed friend crow to take",item,"and add it to ur inventory")
+
+def speak(word): # Talk to a character
+	subject = word[1]
+	if word[1] in ["to"]:
+		subject = word[2]
+	# TODO: table for guys + esp crow friend
+	print("you try to ask the",subject,"something but ur not the same typa critter so it dont work")
+
+def throw(item): # Throw item, always fails
+	print("even friend crow does not have hands to throw a",item)
+
+def wear(item): # Wear item, always fails
+	print("since youre both critters, you have no idea how to wear a",item)
--- IT 140/test_game_main.py
+++ IT 140/test_game_main.py
@@ -1,30 +1,69 @@
-import friend_crow
-import ref
+import friend_crow #the diegetic "verbs" the player can do
+import ref #descripts, dictionaries, and other things that add flavor
 
-# functions!
+# FUNCTIONS
+# TODO: remove testing print, double-check prompt wording, double-check synonyms, update text for each non-friend_crow choice. also maybe something better than a big elif lol, formatting output
 
+# Core loop | captures prompt and sends to parser
 def next_prompt():
-	prompt = input("What's your next move?\n").lower()
+	prompt = input("↦ What's your next move?\n").lower()
 	parsing(prompt)
 
+# Parser | breaks up multi-word prompts into array (if applicable), then checks for matching verbs/nouns, ends with asking for next prompt
+#TODO: needs a break for win condition
 def parsing(prompt):
-	if prompt in ["help","halp","help plz","plz","pls"]:
-		friend_crow.help(prompt)
-		next_prompt()
-	elif prompt[slice(7)] in ["examine"]:
-		lookup_noun = prompt[slice(8,len(prompt))]
-		print("youre gonna look up the descript for", lookup_noun,"\n")
-		next_prompt()
+	#TODO: FOR TESTING ONLY
+	print("--->This is the current prompt:",prompt,"<---")
+	#######
+	split_prompt = prompt.split(" ")
+	#######
+	####### 1 word prompts
+	if split_prompt[0] in ["help","halp","help plz","plz","pls","clue","hint"]:
+		friend_crow.help()
+	elif prompt in ["verbs","v","verb"]:
+		friend_crow.verbs()
+	elif prompt in ["north","south","east","west","n","s","e","w"]:
+		friend_crow.move(prompt)
+	elif prompt in ["inventory","i"]:
+		print("this is where the inventory array would be displayed all nice")
+	elif prompt in ["sleep","wait","z"]:
+		print("if there were turn counters, this would matter!")
+	elif prompt in ["about","credits"]:
+		print("its just me lol")
+	elif prompt in ["load", "save", "score"]:
+		print("this is a pretty simple game, so there's none of that here! sorry :(")
+	elif prompt in ["quit","exit"]:
+		print("idk what ur using lol but if this is bash/zsh you can ctrl+C, but you probably already knew that!")
+	#######
+	####### 2+ word prompts
+	elif split_prompt[0] in ["examine","x"]:
+		friend_crow.examine(split_prompt[1])
+	elif split_prompt[0] in ["eat","drink"]:
+		friend_crow.eat(split_prompt[1])
+	elif split_prompt[0] in ["drop"]:
+		friend_crow.drop(split_prompt[1])
+	elif split_prompt[0] in ["take","t","get","collect"]:
+		friend_crow.take(split_prompt[1])
+	elif split_prompt[0] in ["talk","greet","speak","ask"]:
+		friend_crow.speak(split_prompt)
+	elif split_prompt[0] in ["throw","toss"]:
+		friend_crow.throw(split_prompt[1])
+	elif split_prompt[0] in ["wear"]:
+		friend_crow.wear(split_prompt[1])
+	elif len(split_prompt) >= 2 and split_prompt[1] in ["north","south","east","west","n","s","e","w"]:
+		friend_crow.move(split_prompt[1])
 	else:
-		print("Friend crow cannot help you here","\n")
-		next_prompt()
+		print("Friend crow cannot help you here, since this is an erorr message","\n")
+	#######
+	next_prompt()
 
 
 # game logic
 
-# game init
-prompt = input("What's your next move?\n").lower()
-parsing(prompt)
+
+# Game initialization
+print(ref.world_init_descript)
+next_prompt()
 
 
 # losing state
